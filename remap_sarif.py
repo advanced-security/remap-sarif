51148fc4015e6c7f5b3a6088d9a03afe660a647f
aegilops <41705651+aegilops@users.noreply.github.com>
2023-04-18 11:15:14 +0100

    Minor improvements, docs, args

--- remap_sarif.py
+++ remap_sarif.py
@@ -1,48 +1,59 @@
 #!/usr/bin/env python3
+"""Remap a SARIF file with sourcemaps."""
 
+import sys
 import argparse
-from dataclasses import replace
 import json
 import logging
 import os
-from textwrap import indent
 import sourcemap
 
 LOG = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
 
 
 def add_args(parser):
     """Add arguments to command line parser."""
-    parser.add_argument('sarif', help="A SARIF file to remap with a sourcemap")
+    parser.add_argument('sarif', help="A SARIF file to remap with sourcemaps")
     parser.add_argument('sourceroot', help="The source root for the SARIF file")
+    parser.add_argument('--output',
+                        required=False,
+                        help="The output SARIF file to write to")
+    parser.add_argument('--debug',
+                        '-d',
+                        action="store_true",
+                        help="Debug output on")
 
 
 class Mapper:
+    """Remap source file locations with a source root and a cache of source maps."""
+
     def __init__(self, sourceroot) -> None:
         self.root = sourceroot
-        self.cache = {}
+        self.cache: dict[str, sourcemap.objects.SourceMapIndex] = {}
 
-    def remap(self, filepath, line, col) -> tuple[str, int, int]:
+    def remap(self, filepath, line, col) -> tuple[str, str, int, int]:
+        """Remap the locations."""
         if filepath in self.cache:
             smap = self.cache["file"]
         else:
             try:
-                map_file = sourcemap.discover(open(os.path.join(self.root, filepath)).read())
+                map_file = sourcemap.discover(
+                    open(os.path.join(self.root, filepath)).read())
             except IOError as err:
-                raise IndexError("Mapping not found: %s", err)
+                raise IndexError("Mapping error: %s", err)
             if map_file is not None:
                 with open(os.path.join(self.root, map_file)) as f:
                     smap = sourcemap.load(f)
                     self.cache["file"] = smap
             else:
-                raise IndexError(f"Mapping not found: map file for {filepath} not found")
+                raise IndexError(
+                    f"Mapping error: map file for {filepath} not found")
         try:
             loc = smap.lookup(line, col)
-        except IndexError as err:
-            raise IndexError(f"Mapping not found: {filepath}!{line}:{col} not found")
+        except IndexError:
+            raise IndexError(
+                f"Mapping error: {filepath}!{line}:{col} not found")
         return (loc.name, loc.src, loc.src_line, loc.src_col)
-
 
 
 def main():
@@ -51,10 +62,16 @@ def main():
     add_args(parser)
     args = parser.parse_args()
 
+    logging.basicConfig(level=logging.INFO)
+
+    if args.debug:
+        LOG.setLevel(logging.DEBUG)
+
     sarif_file = args.sarif
     mapper = Mapper(args.sourceroot)
 
-    sarif = json.load(open(sarif_file))
+    with open(sarif_file, "r") as f:
+        sarif = json.load(f)
 
     try:
         for run in sarif["runs"]:
@@ -63,20 +80,25 @@ def main():
 
             for result in run["results"]:
                 for location in result["locations"]:
-                    uri = location["physicalLocation"]["artifactLocation"]["uri"]
+                    uri = location["physicalLocation"]["artifactLocation"][
+                        "uri"]
                     region = location["physicalLocation"]["region"]
 
                     try:
                         startline = region["startLine"]
                         startcol = region["startColumn"]
-                        name, src, src_line, src_col = mapper.remap(
-                            uri, startline, startcol
-                        )
 
-                        LOG.debug("Found mapping to %s @ %s!%s:%s for %s!%s:%s", name, src, src_line, src_col, uri, startline, startcol)
+                        name, src, src_line, src_col = mapper.remap(
+                            uri, startline, startcol)
+
+                        LOG.debug("Found mapping to %s @ %s!%s:%s for %s!%s:%s",
+                                  name, src, src_line, src_col, uri, startline,
+                                  startcol)
 
                         if src is not None:
-                            location["physicalLocation"]["artifactLocation"]["uri"] = src
+                            location["physicalLocation"]["artifactLocation"][
+                                "uri"] = src
+
                             region["startLine"] = src_line
                             region["startColumn"] = src_col
 
@@ -85,18 +107,22 @@ def main():
                                     if artifact["location"]["uri"] == uri:
                                         artifact["location"]["uri"] = src
                                         replaced_artifacts.add(uri)
-                            
+
                     except IndexError as err:
                         LOG.warning("Index error: %s", err)
 
                     try:
-                        endline = region["endLine"] if "endLine" in region else startline
-                        endcol = region["endColumn"] if "endColumn" in region else startcol
-                        name, src, src_line, src_col = mapper.remap(
-                            uri, endline, endcol
-                        )
+                        endline = region[
+                            "endLine"] if "endLine" in region else startline
+                        endcol = region[
+                            "endColumn"] if "endColumn" in region else startcol
 
-                        LOG.debug("Found mapping to %s @ %s!%s:%s for %s!%s:%s", name, src, src_line, src_col, uri, endline, endcol)
+                        name, src, src_line, src_col = mapper.remap(
+                            uri, endline, endcol)
+
+                        LOG.debug("Found mapping to %s @ %s!%s:%s for %s!%s:%s",
+                                  name, src, src_line, src_col, uri, endline,
+                                  endcol)
 
                         if src is not None:
                             region["endLine"] = src_line
@@ -106,7 +132,9 @@ def main():
     except KeyError as err:
         LOG.warning("Malformed SARIF, expected key missing: %s", err)
 
-    print(json.dumps(sarif, indent=2))
+    # note: closes STDOUT if we're writing to there
+    with open(args.output, "w") if args.output else sys.stdout as f:
+        print(json.dumps(sarif, indent=2), file=f)
 
 
 if __name__ == "__main__":
